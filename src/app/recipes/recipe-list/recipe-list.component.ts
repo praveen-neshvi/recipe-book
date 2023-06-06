9b80bfc0908a2ef9ca87e9d32b34252c54cc0c03
Kumar N P, Praveen <Praveen.KumarNP@boeing.com>
2023-06-06 18:49:28 +0530

    Front-page UI craeted

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', ' This is simply a test', 
+    'https://www.simplyrecipes.com/thmb/4_wTLA6Q_2o7dgNO-ze0w7oeX_w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg'),
+    new Recipe('A test recipe', ' This is simply a test', 
+    'https://sipbitego.com/wp-content/uploads/2021/08/Pepperoni-Pizza-Recipe-Sip-Bite-Go.jpg')
+   ];
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-recipes',
     templateUrl: './recipes.component.html',
-    styles: ['./recipes.component.css']
+    styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent{
 
